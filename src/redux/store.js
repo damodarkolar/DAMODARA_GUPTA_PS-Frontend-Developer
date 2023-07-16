bed78c0dd5c1f1cbf7f2662558c4da626289d2cb
damodarkolar <damodarkolar@gmail.com>
2023-07-16 21:10:08 +0530

    add

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { AuthReducer } from "./Auth/authReducer";
+import { legacy_createStore as createStore ,combineReducers, applyMiddleware, compose} from "redux";
+import { CartReducer } from "./Cart/CartReducer";
+import thunk  from "redux-thunk"
+const rootReducer =combineReducers({
+    Auth:AuthReducer,
+    Cart: CartReducer
+})
+
+export const store= createStore(rootReducer,compose(applyMiddleware(thunk)))
